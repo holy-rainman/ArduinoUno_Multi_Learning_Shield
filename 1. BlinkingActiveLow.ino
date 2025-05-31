fcac76629045481b193474a57d1eeaa4b43dc07a
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2025-05-31 17:38:12 +0800

    Update 1. BlinkingActiveLow.ino

--- 1. BlinkingActiveLow.ino
+++ 1. BlinkingActiveLow.ino
@@ -1,9 +1,9 @@
 void setup() 
 { pinMode(13,OUTPUT);
 }
-void loop() 
+void loop() 
 { digitalWrite(13,LOW);
-  delay(500);
-  digitalWrite(13,HIGH);
-  delay(500);
+  delay(500);
+  digitalWrite(13,HIGH);
+  delay(500);
 }
